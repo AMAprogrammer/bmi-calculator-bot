52c16e27826155668ba6d85a68d4a9b87af322da
Amirhossein Alizadeh <amirhosseinalizadeh992@gmail.com>
2024-02-02 18:32:52 +0330

    commit-3

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 from keyboards import main_keyboard
 from database_config import bmi
+from tools import calculator
 
 
 
@@ -28,3 +29,40 @@ async def main_menu(client, message:Message):
 
 @bot.on_message(filters=filters.private & filters.regex("محاسبه BMI من"))
 async def calculate_bmi(client, message:Message):
+    
+    user_id = message.from_user.id
+    text = message.text
+    users = bmi["USERS"]
+    user = users.find_one({"_id":user_id})
+    if user:
+        user_step = user["step"]
+        if user_step == "home":
+            
+            await bot.send_message(chat_id=user_id, text="قد خود را وارد کنید:")
+            users.update_one({"_id":user_id},{"$set":{"step":"get_height"}})
+            
+        elif user_step == "get_height":
+            
+            try:
+                height = float(text)
+            except:
+                await bot.send_message(chat_id=user_id, text="لطفا قد خود را با فرمت صحیح ارسال کنید!")
+            else:
+                users.update_one({"_id":user_id},{"$set":{"height":height}})
+                users.update_one({"_id":user_id},{"$set":{"step":"get_weight"}})
+                await bot.send_message(chat_id=user_id, text="وزن خود را وارد کنید:")
+
+        elif user_step == "get_weight":
+            
+            try:
+                weight = float(text)
+            except:
+                await bot.send_message(chat_id=user_id, text="لطفا وزن خود را با فرمت صحیح ارسال کنید!")
+            else:  
+                users.update_one({"_id":user_id},{"$set":{"weight":weight}})
+                users.update_one({"_id":user_id},{"$set":{"step":"home"}})
+                user = users.find_one({"_id":user_id})
+                height = user["height"]
+                weight = user["weight"]
+                result = calculator(height=height, weight=weight)
+                await bot.send_message(chat_id=user_id, text=f"BMI شما:\n\n{result}\n\nبرای بازگشت به منوی اصلی /start را بزنید")                
